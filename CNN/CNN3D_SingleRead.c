4a66a89def36662e772acff94c8103641871723d
tianweixing <stevenxtw@gmail.com>
2016-05-24 14:49:35 -0700

    creating CNN network, short of memory

---
+++ CNN/CNN3D_SingleRead.c
@@ -0,0 +1,4 @@
+// we set all images/kernels value to float type
+// in binary implementation, they should have only binary values.
+// due to memory limitation, we read the weights of a single neuron
+// from file every time we use them
